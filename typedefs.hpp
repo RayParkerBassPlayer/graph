eaf04842da0a86f917b4f9ba4aa4b198421b93b9
Ray Parker <RayParkerBassPlayer@gmail.com>
2019-04-27 14:46:40 -0400

    IdList added.

--- typedefs.hpp
+++ typedefs.hpp
@@ -2,10 +2,16 @@
 #define __GRAPH_TYPEDEFS_HPP
 
 #include <vector>
+#include <list>
+#include <string>
 
 using std::vector;
+using std::list;
+using std::string;
 
 class Node;
 typedef vector<Node *> NodeVector;
+
+typedef list<string> IdList;
 
 #endif
