b82c041758c9eaba5460dceeefcdfe02eff1830c
Ray Parker <RayParkerBassPlayer@gmail.com>
2020-02-02 13:43:22 -0500

    Testing against dirty graph.

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,7 +2,10 @@
 #include "../node.hpp"
 #include "../Graph.hpp"
 
+Graph *graph;
+
 int main(void){
+  graph = new Graph();
   SpecSuite nodeSuite("Node structure and validation");
 
   nodeSuite.Spec("Setters and getters properly store values.", [](){
@@ -20,71 +23,67 @@ int main(void){
   SpecSuite graphSpecs("Graph manipulation");
 
   graphSpecs.Spec("Adds a node below the root", [](){
-        Graph graph;
         Node *childNode = new Node("2nd Level", "Some Value");
         string childID = childNode->ID();
 
-        graph.AddNode(childNode);
+        graph->AddNode(childNode);
         
-        expect(graph.Root()->Children().size() == 1);
-        expect(graph.Root()->Children().front()->ID() == childID);
+        expect(graph->Root()->Children().size() == 1);
+        expect(graph->Root()->Children().front()->ID() == childID);
       });
 
   graphSpecs.Spec("Inserts a node between two others", [](){
-        Graph graph;
         Node *olderNode = new Node("OldNode"),
              *newerNode = new Node("Newer Node");
+             NodeVector::size_type initialRootChildren = graph->Root()->Children().size();
 
-        graph.AddNode(olderNode);
-        graph.InsertNode(graph.Root(), newerNode, olderNode);
+        graph->AddNode(olderNode);
+        graph->InsertNode(graph->Root(), newerNode, olderNode);
 
-        expect(graph.Root()->Children().size() == 1);
-        expect(graph.Root()->Children().front() == newerNode);
+        expect(graph->Root()->Children().size() == initialRootChildren + 1);
+        // Can't depend on location here.  Index is sorted.
+        expect(graph->Root()->Children().back() == newerNode);
         expect(olderNode->Children().size() == 0);
         expect(olderNode->Parents().front() == newerNode);
       });
 
   graphSpecs.Spec("Properly adds an node to the mother index only once", [](){
-        Graph graph;
-
-        expect(graph.Index().size() == 1);
+        int initialSize = graph->Index().size();
 
         Node *addManyTimes = new Node(),
              *anotherNode = new Node();
 
-        graph.AddNode(addManyTimes);
-        expect(graph.Index().size() == 2);
+        graph->AddNode(addManyTimes);
+        expect(graph->Index().size() == initialSize + 1);
 
-        graph.AddNode(anotherNode);
-        expect(graph.Index().size() == 3);
+        graph->AddNode(anotherNode);
+        expect(graph->Index().size() == initialSize + 2);
 
         // Adding node to Graph again with different relationships, 
         // but it's already in the index and should not be added to the index again.
-        graph.AddNode(anotherNode, addManyTimes);
-        expect(graph.Index().size() == 3);
+        graph->AddNode(anotherNode, addManyTimes);
+        expect(graph->Index().size() == initialSize + 2);
       });
 
   graphSpecs.Spec("Finds a node in its index", [](){
-        Graph graph;
         Node *toFind = new Node();
 
-        expect(graph.FindNode(toFind->ID()) == &NULL_NODE);
+        expect(graph->FindNode(toFind->ID()) == &NULL_NODE);
 
-        graph.AddNode(toFind);
-        expect(graph.FindNode(toFind->ID()) == toFind);
+        graph->AddNode(toFind);
+        expect(graph->FindNode(toFind->ID()) == toFind);
       });
 
   graphSpecs.Spec("Returns a path for a simple path", [](){
-        Graph graph;
         Node *level2 = new Node(), 
              *level3 = new Node(), 
              *level4 = new Node();
 
-        graph.AddNode(level2);
-        graph.AddNode(level2, level3);
-        graph.AddNode(level3, level4);
+        graph->AddNode(level2);
+        graph->AddNode(level2, level3);
+        graph->AddNode(level3, level4);
 
-        GraphPaths *paths = graph.Paths(level4);
+        GraphPaths *paths = graph->Paths(level4);
 
         expect(paths != NULL);
         expect(paths->size() == 1);
@@ -96,7 +95,7 @@ int main(void){
         ids.push_back(level4->ID());
         ids.push_back(level3->ID());
         ids.push_back(level2->ID());
-        ids.push_back(graph.Root()->ID());
+        ids.push_back(graph->Root()->ID());
 
         int o = 0;
 
@@ -109,16 +108,15 @@ int main(void){
       });
 
   graphSpecs.Spec("Returns paths for a multiple paths", [](){
-        Graph graph;
         Node *level2 = new Node(), 
              *level3 = new Node(), 
              *level4 = new Node();
 
-        graph.AddNode(level2);
-        graph.AddNode(level2, level3);
-        graph.AddNode(level3, level4);
+        graph->AddNode(level2);
+        graph->AddNode(level2, level3);
+        graph->AddNode(level3, level4);
 
-        GraphPaths *paths = graph.Paths(level4);
+        GraphPaths *paths = graph->Paths(level4);
 
         expect(paths != NULL);
         expect(paths->size() == 1);
@@ -128,14 +126,13 @@ int main(void){
       });
 
   graphSpecs.Spec("Returns siblings for a node in the simple case", [](){
-        Graph graph;
         Node *sibling1 = new Node(),
              *sibling2 = new Node(),
              *sibling3 = new Node();
 
-        graph.AddNode(sibling1);
-        graph.AddNode(sibling2);
-        graph.AddNode(sibling3);
+        graph->AddNode(sibling1);
+        graph->AddNode(sibling2);
+        graph->AddNode(sibling3);
 
         NodeList list;
 
@@ -144,9 +141,7 @@ int main(void){
         list.push_back(sibling2);
         list.push_back(sibling3);
 
-        NodeList *siblings = graph.Siblings(sibling1);
-
-        expect(siblings->size() == 3);
+        NodeList *siblings = graph->Siblings(sibling1);
 
         // Go through all siblings and make sure that they are returned from the graph, including sibling1.
         for(Node *sibling : list){
@@ -159,7 +154,6 @@ int main(void){
       });
 
   graphSpecs.Spec("Returns siblings for a node in a multi-parent case", [](){
-        Graph graph;
         Node *sibling1 = new Node(),
              *sibling2 = new Node(),
              *sibling3 = new Node(),
@@ -169,16 +163,16 @@ int main(void){
              *parent1 = new Node(),
              *parent2 = new Node();
 
-        graph.AddNode(parent1);     
-        graph.AddNode(parent2);     
+        graph->AddNode(parent1);     
+        graph->AddNode(parent2);     
 
-        graph.AddNode(parent1, sibling1);
-        graph.AddNode(parent1, sibling2);
-        graph.AddNode(parent1, sibling3);
+        graph->AddNode(parent1, sibling1);
+        graph->AddNode(parent1, sibling2);
+        graph->AddNode(parent1, sibling3);
 
-        graph.AddNode(parent2, sibling4);
-        graph.AddNode(parent2, sibling5);
-        graph.AddNode(parent2, sibling6);
+        graph->AddNode(parent2, sibling4);
+        graph->AddNode(parent2, sibling5);
+        graph->AddNode(parent2, sibling6);
 
         // This is the connection -- siblings are all connected by sibling1 having common parents with all other siblings.
         sibling1->AddParent(parent2);
@@ -193,7 +187,7 @@ int main(void){
         list.push_back(sibling5);
         list.push_back(sibling6);
 
-        NodeList *siblings = graph.Siblings(sibling1);
+        NodeList *siblings = graph->Siblings(sibling1);
 
         expect(siblings->size() == 6);
 
@@ -211,10 +205,10 @@ int main(void){
   //       Node *secondLevel = new Node("2nd Level"),
   //            *thirdLevel = new Node("3rd Level");
   //
-  //       graph.AddNode(secondLevel);
-  //       graph.AddNode(secondLevel, thirdLevel);
+  //       graph->AddNode(secondLevel);
+  //       graph->AddNode(secondLevel, thirdLevel);
   //
-  //       paths = graph.PathTo(thirdLevel);
+  //       paths = graph->PathTo(thirdLevel);
   //
   //     });
 
