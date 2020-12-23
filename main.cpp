d33f798d8bae66b6d5f4e723144e1ad1ba3cae6d
Ray Parker <RayParkerBassPlayer@gmail.com>
2020-12-22 21:22:36 -0500

    DB connection testing.  Connecting, but not authing.

--- main.cpp
+++ main.cpp
@@ -2,22 +2,44 @@
 #include "global_includes.hpp"
 #include "Graph.hpp"
 
+#include <mysql_connection.h>
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+
 using namespace std;
+using namespace sql;
 
 int main(void){
   spdlog::set_level(spdlog::level::debug);
 
   spdlog::info("Graph starting up.");
-  Graph *graph = new Graph();
+  // Graph *graph = new Graph();
+  //
+  // spdlog::info("Loading graph...");
+  //
+  // for(int i = 0; i < 100; i++)
+  //   graph->AddNode(new Node());
+  //
+  // spdlog::info("...finished loading Graph");
+  //
+  // delete graph;
 
-  spdlog::info("Loading graph...");
+  spdlog::info("Getting driver");
+  sql::Driver *driver = get_driver_instance();
 
-  for(int i = 0; i < 100; i++)
-    graph->AddNode(new Node());
+  spdlog::info("Connecting to db");
+  sql::Connection *connection = driver->connect("tcp://localhost:3306", "graph", "password");
 
-  spdlog::info("...finished loading Graph");
+  sql::Statement *statement = connection->createStatement();
+  statement->execute("use graph");
+  statement->execute("drop table if exists test");
+  statement->execute("CREATE TABLE test(id INT, label CHAR(1))");
+  statement->execute("INSERT INTO test(id, label) VALUES (1, 'a')");
 
-  delete graph;
+  spdlog::info("Closing DB connection.");
+  delete connection;
 
   return 0;
 }
